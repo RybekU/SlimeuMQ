d85b40d09a024f79e285bb4501f1da996df79077
RybekU <45723292+RybekU@users.noreply.github.com>
2020-10-13 04:13:46 +0200

    make sprites flippable on x axis

--- src/gfx/mod.rs
+++ src/gfx/mod.rs
@@ -14,6 +14,8 @@ pub struct Sprite {
     pub rect: Rect,
     /// offset from the location given by Position component, by default the center
     pub offset: Vec2,
+    /// if true sprite faces left
+    pub flip: bool,
 }
 
 impl Sprite {
@@ -23,6 +25,7 @@ impl Sprite {
             src: name,
             rect: Rect::new(x, y, width, height),
             offset: -Vec2::new(width, height) / 2.,
+            flip: false,
         }
     }
 }
@@ -34,13 +37,23 @@ pub fn render(game: &Game) {
     let mut query = <(&Position, &Sprite)>::query();
     for (position, sprite) in query.iter(&game.world) {
         let texture = game.textures.get(&sprite.src).unwrap();
+        let rect = if sprite.flip {
+            Rect::new(
+                sprite.rect.x + sprite.rect.w,
+                sprite.rect.y,
+                -sprite.rect.w,
+                sprite.rect.h,
+            )
+        } else {
+            sprite.rect
+        };
         draw_texture_ex(
             *texture,
             position.src.x() + sprite.offset.x(),
             position.src.y() + sprite.offset.y(),
             WHITE,
             DrawTextureParams {
-                source: Some(sprite.rect),
+                source: Some(rect),
                 ..Default::default()
             },
         );
